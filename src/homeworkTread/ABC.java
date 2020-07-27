2f1e33b3783ec7e87128c255c10df2e6975e1c13
NikKonshin <konbshin1996@gmail.c0m>
2020-07-27 15:58:18 +0300

    hw3_abc

---
+++ src/homeworkTread/ABC.java
@@ -0,0 +1,66 @@
+package homeworkTread;
+
+public class ABC {
+    static Object monitor = new Object();
+    static volatile int currentNum = 1;
+    static final int NUM = 5;
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            try {
+            for (int i = 0; i <NUM ; i++) {
+                synchronized (monitor) {
+
+                        while (currentNum != 1) {
+                            monitor.wait();
+                        }
+                        System.out.print("A");
+                        currentNum = 2;
+                        monitor.notifyAll();
+                    }
+                }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+        }).start();
+
+        new Thread(()->{
+            try {
+                for (int i = 0; i <NUM ; i++) {
+                    synchronized (monitor) {
+
+                        while (currentNum != 2) {
+                            monitor.wait();
+                        }
+                        System.out.print("B");
+                        currentNum = 3;
+                        monitor.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(()->{
+            try {
+                for (int i = 0; i <NUM ; i++) {
+                    synchronized (monitor) {
+
+                        while (currentNum != 3) {
+                            monitor.wait();
+                        }
+                        System.out.print("C");
+                        currentNum = 1;
+                        monitor.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+
+    }
+
+}
